bc1003bdeff24d0a3de91936f3bffbcdc55df21d
cloudwolf <206946888+xerxes324@users.noreply.github.com.>
2026-02-28 17:38:12 +0530

    add async CLI entrypoint for mcp agent

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-def main():
-    print("Hello from mcp-couchbase!")
+import asyncio
+from agent.agent import build_agent
 
+async def main():
+    async with build_agent() as agent: # buildagent is an ACM now. so async with -> enter it and get yielded agent whilst keeping its resources alive
+        config = {"configurable" : {"thread_id" : "1"}}
+        
+        while True:
+            userinput = input("\nYou: ")
+            if userinput.lower() == "exit":
+                break
+            
+            result = await agent.ainvoke({"messages" : userinput}, config)
+            print(f"\nAgent: {result['messages'][-1].content}")
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
